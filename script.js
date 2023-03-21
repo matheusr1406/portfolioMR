1c3e84732090c793ae3f638a3f85332373f94ed3
matheusrocha1406 <mr140603@gmail.com>
2023-03-21 18:23:21 -0300

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-text',{delay: 200, origin:'top'})
+sr.reveal('.home-img',{delay: 400, origin:'top'})
+sr.reveal('.about, .services, .cta, .resume, .contact, .redes',{delay: 200, origin:'top'})
