190f29f83d81268995866aeed521ec83a3435021
matheusrocha1406 <mr140603@gmail.com>
2023-03-24 13:42:22 -0300

    commit 4

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navabr');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 const sr = ScrollReveal ({
